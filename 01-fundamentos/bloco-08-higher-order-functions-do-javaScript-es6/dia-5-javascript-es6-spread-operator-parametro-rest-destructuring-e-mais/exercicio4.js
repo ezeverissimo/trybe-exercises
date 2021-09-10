334ae918578efaf39cb6cf2dc7535d0f58d4da9f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-10 19:05:26 -0300

    Adiciona exercicios 1 2 3 4

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javaScript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio4.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = (any) => any.filter(
+    ({bornIn, nationality}) => {
+    if (bornIn <= 2000 && bornIn > 1900 && nationality === 'Australian') {
+        return any
+    }
+})
+
+
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
