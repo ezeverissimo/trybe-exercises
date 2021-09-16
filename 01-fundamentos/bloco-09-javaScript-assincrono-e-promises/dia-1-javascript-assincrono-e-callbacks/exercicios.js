7c7fd372ab385b84eecd363d5ad219720286480f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-16 09:47:16 -0300

    exercicio 1

---
+++ 01-fundamentos/bloco-09-javaScript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/exercicios.js
@@ -0,0 +1,14 @@
+// const getPlanet = () => {
+//     const mars = {
+//       name: "Mars",
+//       distanceFromSun: {
+//         value: 227900000,
+//         measurementUnit: "kilometers",
+//       },
+//     };
+//     console.log("Returned planet: ", mars.name);
+//   };
+  
+  
+//   setTimeout(() => getPlanet(), 4000);
+//   console.log('deu certo');
