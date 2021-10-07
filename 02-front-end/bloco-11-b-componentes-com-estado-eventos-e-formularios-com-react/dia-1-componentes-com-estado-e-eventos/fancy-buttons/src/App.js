15244ade8590b627f87cccf2ab0d91dca12af179
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-10-07 11:12:18 -0300

    adiciona pasta react e implementa primeiro exercicio fixador

---
+++ 02-front-end/bloco-11-b-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+/* Embora isso funcione, essa DEFINITIVAMENTE
+não é a maneira correta de se criar eventos
+em React! A função se refere ao componente,
+então deve ser parte de sua classe! */
+function handleClick1() {
+  console.log('Clicou no botão! 1')
+}
+function handleClick2() {
+  console.log('Clicou no botão! 2')
+}
+function handleClick3() {
+  console.log('Clicou no botão! 3')
+}
+
+class App extends React.Component {
+  /* Repare que, diferentemente do HTML, no
+  JSX você associa uma função a um evento
+  passando a própria função entre chaves `{}` */
+  render() {
+    return (
+      <div>
+        <button onClick={handleClick1}>Meu botão</button>
+        <button onClick={handleClick2}>Meu botão</button>
+        <button onClick={handleClick3}>Meu botão</button>
+      </div>
+    )
+  }
+}
+
+export default App;
