7f0384d4ca955ab17b6ef837e51c143243c1d088
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-10-30 15:30:06 -0300

    adiciona

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// App.js
+import React, { Component } from 'react';
 import './App.css';
+import ValidEmail from './ValidEmail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+          <input
+            id="id-email"
+            value={ email }
+            type="email"
+            onChange={ (e) => this.changeEmail(e.target.value) }
+          />
+        </label>
+        <input
+          id="btn-enviar"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={ () => this.changeSaveEmail(email) }
+        />
+        <input id="btn-id" type="button" value="Voltar" />
+        <ValidEmail email={ saveEmail } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -1,8 +1,25 @@
+// App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+
+// Adicione esse teste.
+test('Verificando se o botão e o campo email estão funcionando.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
 });
