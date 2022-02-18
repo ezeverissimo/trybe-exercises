c953291eb73519514bceb2e7741be2989809855a
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-02-18 18:41:09 -0300

    adiciona variaveis de ambiente

--- 03-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
+++ 03-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const user = require('./controller');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.post('/user', user.create);
 
