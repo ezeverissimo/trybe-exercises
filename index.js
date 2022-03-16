6ca929c604b8b079aee4317d7d0351e730da0ed8
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-03-16 18:58:03 -0300

    set environment variables

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const MESSAGE = process.env.MESSAGE || 'Erro na variavel de ambiente message!';
+
 app.get('/', (req, res) => {
-  return res.status(200).send('Está vivo!!!');
+  return res.status(200).json({ message: MESSAGE });
 });
 
 app.listen(PORT, () => {
